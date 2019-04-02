12807c0d6df91d6ffcf7a52f01fd5f197002384d
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-04-02 16:17:56 +0900

    dwa

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @objc var count = 1
+    var count1 = 1
+    var count2 = 1
+    var myTimer = Timer()
+    var speed = true
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeLabel1: UILabel!
+    @IBOutlet weak var timeLabel2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = "0"
+        timeLabel1.text = "0  :"
+        timeLabel2.text = "0  :"
     }
 
 
+    @IBAction func btnplay(_ sender: Any) {
+        if speed == true{
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        }
+        print(count)
+        speed = false
+    }
+    
+    @IBAction func btnpause(_ sender: Any) {
+        myTimer.invalidate()
+        speed = true
+    }
+    
+    
+    @IBAction func btnstop(_ sender: Any) {
+        myTimer.invalidate()
+        speed = true
+        count = 0
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+    }
+    @objc func updateTime(){
+        if count == 4 {
+            timeLabel1.text = String("\(count1)  :")
+            count1 += 1
+            count = 0
+        }
+        else if count1 == 4{
+            timeLabel2.text = String("\(count2)  :")
+            count2 += 1
+            count1 = 0
+        }
+        timeLabel.text = String(count)
+        count += 1
+    }
 }
 
